429209cedb0d4ec14cb41099115f109b233daf9f
Sultanxda <sultanxda@gmail.com>
2013-05-01 22:12:55 -0700

    Fixed compile errors

--- kernel/power/earlysuspend.c
+++ kernel/power/earlysuspend.c
@@ -35,10 +35,6 @@ static int debug_mask = DEBUG_USER_STATE | DEBUG_NO_SUSPEND;
 static int debug_mask = DEBUG_USER_STATE;
 #endif
 
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-atomic_t optimize_comp_on = ATOMIC_INIT(0);
-EXPORT_SYMBOL(optimize_comp_on);
-#endif /* CONFIG_ZRAM_FOR_ANDROID */
 module_param_named(debug_mask, debug_mask, int, S_IRUGO | S_IWUSR | S_IWGRP);
 
 static DEFINE_MUTEX(early_suspend_lock);
@@ -100,10 +96,7 @@ static void early_suspend(struct work_struct *work)
 	pr_info("[R] early_suspend start\n");
 	mutex_lock(&early_suspend_lock);
 	spin_lock_irqsave(&state_lock, irqflags);
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-	atomic_set(&optimize_comp_on, 1);
-#endif /* CONFIG_ZRAM_FOR_ANDROID */
-	if (state == SUSPEND_REQUESTED)
+	if (state == SUSPEND_REQUESTED) {
 		state |= SUSPENDED;
 #ifdef CONFIG_HTC_ONMODE_CHARGING
 		state_onchg = SUSPEND_REQUESTED_AND_SUSPENDED;
@@ -155,10 +148,7 @@ static void late_resume(struct work_struct *work)
 	pr_info("[R] late_resume start\n");
 	mutex_lock(&early_suspend_lock);
 	spin_lock_irqsave(&state_lock, irqflags);
-#ifdef CONFIG_ZRAM_FOR_ANDROID
-	atomic_set(&optimize_comp_on, 0);
-#endif /* CONFIG_ZRAM_FOR_ANDROID */
-	if (state == SUSPENDED)
+	if (state == SUSPENDED) {
 		state &= ~SUSPENDED;
 #ifdef CONFIG_HTC_ONMODE_CHARGING
 		state_onchg &= ~SUSPEND_REQUESTED_AND_SUSPENDED;
