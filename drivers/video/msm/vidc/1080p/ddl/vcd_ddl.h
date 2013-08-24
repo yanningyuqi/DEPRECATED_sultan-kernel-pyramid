d99d17403713340e625c2af807c61399a624697c
Sultanxda <sultanxda@gmail.com>
2013-08-24 12:10:35 -0700

    Revert "Import vidc from CAF msm-AU_LINUX_ANDROID_JB_CHOCOLATE.04.01.02.001.138"
    This reverts commit a0030fd0d738e224ef7aa90e168afa8ba81ef1f3.

--- drivers/video/msm/vidc/1080p/ddl/vcd_ddl.h
+++ drivers/video/msm/vidc/1080p/ddl/vcd_ddl.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2010-2012, Code Aurora Forum. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -100,7 +100,6 @@ struct ddl_buf_addr{
 	struct ion_handle *alloc_handle;
 	u32 buffer_size;
 	enum ddl_mem_area mem_type;
-	void *pil_cookie;
 };
 enum ddl_cmd_state{
 	DDL_CMD_INVALID         = 0x0,
@@ -480,5 +479,5 @@ extern u32 vidc_video_codec_fw_size;
 u32 ddl_fw_init(struct ddl_buf_addr *dram_base);
 void ddl_get_fw_info(const unsigned char **fw_array_addr,
 	unsigned int *fw_size);
-void ddl_fw_release(struct ddl_buf_addr *);
+void ddl_fw_release(void);
 #endif
