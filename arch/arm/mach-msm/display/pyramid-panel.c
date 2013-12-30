6290c45ad7680c2fda469d7e21fa36eefd824e4a
Sultanxda <sultanxda@gmail.com>
2013-12-29 23:05:32 -0800

    pyramid: Undervolt display by 400mV
    Credit to scverhagen for the idea.

--- arch/arm/mach-msm/display/pyramid-panel.c
+++ arch/arm/mach-msm/display/pyramid-panel.c
@@ -78,7 +78,7 @@ static void pyramid_panel_power(int on)
 			}
 		}
 
-		ret = regulator_set_voltage(l1_3v, 3100000, 3100000);
+		ret = regulator_set_voltage(l1_3v, 2700000, 2700000);
 		if (ret) {
 			PR_DISP_ERR("%s: error setting l1_3v voltage\n", __func__);
 			goto fail;
