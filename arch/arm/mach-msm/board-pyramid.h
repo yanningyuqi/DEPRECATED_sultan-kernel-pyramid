fc5bf05046035bbf5515bedf8bf10923ea52a2a3
Sultanxda <sultanxda@gmail.com>
2014-01-06 18:16:41 -0800

    pyramid: Increase ION MM & WB size by 5MB

--- arch/arm/mach-msm/board-pyramid.h
+++ arch/arm/mach-msm/board-pyramid.h
@@ -51,10 +51,10 @@
 #define MSM_PMEM_ADSP_SIZE    0x1800000
 
 // ION SMI
-#define MSM_ION_MM_SIZE       0x3000000
+#define MSM_ION_MM_SIZE       0x3500000
 
 // ION
-#define MSM_ION_WB_SIZE       0x2FD000
+#define MSM_ION_WB_SIZE       0x800000
 #define MSM_ION_SF_SIZE       0x29A0000
 #define MSM_ION_MM_FW_SIZE    0x200000
 
@@ -64,15 +64,15 @@
 #define USER_SMI_BASE         (KERNEL_SMI_BASE + KERNEL_SMI_SIZE)
 #define MSM_PMEM_SMIPOOL_BASE USER_SMI_BASE
 #define MSM_ION_SF_BASE       (0x40400000)
-#define MSM_ION_WB_BASE       (0x45C00000)
 #define MSM_PMEM_AUDIO_BASE   (0x46400000)
-#define MSM_ION_MM_FW_BASE    (0x6B600000)
-#define MSM_ION_MM_BASE       (0x6B800000)
+#define MSM_ION_WB_BASE       (0x6A900000)
+#define MSM_ION_MM_FW_BASE    (0x6B100000)
+#define MSM_ION_MM_BASE       (0x6B300000)
 #define MSM_PMEM_ADSP_BASE    (0x6E800000)
 
 // Userspace allocation
 #define PHY_BASE_ADDR1  0x48000000
-#define SIZE_ADDR1      0x23600000
+#define SIZE_ADDR1      0x22900000
 /*** END Memory map ***/
 
 /* GPIO definition */
