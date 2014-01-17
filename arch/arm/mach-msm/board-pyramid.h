11007bd772a8dd909a28952154026e94aca77dd1
Sultanxda <sultanxda@gmail.com>
2014-01-16 17:41:10 -0800

    pyramid: Remap memory for 545MB of RAM
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- arch/arm/mach-msm/board-pyramid.h
+++ arch/arm/mach-msm/board-pyramid.h
@@ -47,16 +47,16 @@
 #define MSM_PMEM_SMIPOOL_SIZE USER_SMI_SIZE
 
 // PMEM
-#define MSM_PMEM_AUDIO_SIZE   0x239000
+#define MSM_PMEM_AUDIO_SIZE   0x28B000
 #define MSM_PMEM_ADSP_SIZE    0x1800000
 
 // ION SMI
-#define MSM_ION_MM_SIZE       0x3500000
+#define MSM_ION_MM_SIZE       0x2A00000
 
 // ION
-#define MSM_ION_WB_SIZE       0x800000
+#define MSM_ION_WB_SIZE       0x2FD000
 #define MSM_ION_SF_SIZE       0x29A0000
-#define MSM_ION_MM_FW_SIZE    0x200000
+#define MSM_ION_MM_FW_SIZE    0x300000
 
 // Base addresses
 #define MSM_SMI_BASE          (0x38000000)
@@ -64,15 +64,15 @@
 #define USER_SMI_BASE         (KERNEL_SMI_BASE + KERNEL_SMI_SIZE)
 #define MSM_PMEM_SMIPOOL_BASE USER_SMI_BASE
 #define MSM_ION_SF_BASE       (0x40400000)
+#define MSM_ION_WB_BASE       (0x45C00000)
 #define MSM_PMEM_AUDIO_BASE   (0x46400000)
-#define MSM_ION_WB_BASE       (0x6A900000)
-#define MSM_ION_MM_FW_BASE    (0x6B100000)
-#define MSM_ION_MM_BASE       (0x6B300000)
+#define MSM_ION_MM_FW_BASE    (0x6BB00000)
+#define MSM_ION_MM_BASE       (0x6BE00000)
 #define MSM_PMEM_ADSP_BASE    (0x6E800000)
 
 // Userspace allocation
 #define PHY_BASE_ADDR1  0x48000000
-#define SIZE_ADDR1      0x22900000
+#define SIZE_ADDR1      0x23B00000
 /*** END Memory map ***/
 
 /* GPIO definition */
